071958287e576d51ab14125148d3aa0cbd1ce68c
mirogon <m1smr@hotmail.com>
2020-04-25 06:44:06 +0200

    Serialize path is now %APPDATA%

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,6 @@
 
 int main(int argc, char** argv)
 {
-
-	/*PATH_BINARY = argv[0];
-	
-	for (int i = PATH_BINARY.size() -1 ; i > 0; --i)
-	{
-		if (PATH_BINARY.at(i) == '/')
-		{
-			break;
-		}
-		else
-		{
-			PATH_BINARY.pop_back();
-			PATH_BINARY.shrink_to_fit();
-		}
-	}
-
-	PATH_BINARY += "TaskHunter.m1data";
-	*/
-
 	//Get %APPDATA% path
 	CHAR rpath[MAX_PATH];
 
@@ -31,16 +12,13 @@ int main(int argc, char** argv)
 		std::cout << "Error: " << result << std::endl;
 		exit(-1);
 	}
-
-	std::cout << rpath << std::endl;
 	
 	//Cast to string and add filename
 	PATH_SERIALIZATION = rpath;
+	PATH_SERIALIZATION += "\\taskhunter.m1data";
 
-	PATH_SERIALIZATION += "\taskhunter.m1data";
-
+	//Start TaskHunter
 	TaskHunter th = TaskHunter();
-
 	th.HandleArgv(argc, argv);	
 
 	return 0;
